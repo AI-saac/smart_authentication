3b63091aa163f2f0aaec35279821a5a9995bc68a
Yuki Hariguchi <y.hariguchi@gmail.com>
2015-10-07 06:28:56 +0900

    valid email regex

--- lib/generators/smart_authentication/templates/app/models/user.rb
+++ lib/generators/smart_authentication/templates/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
