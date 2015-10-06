281ca5eeb1b237a5f0de6bf881984c1ef24f9352
Yuki Hariguchi <y.hariguchi@gmail.com>
2015-10-06 18:42:40 +0900

    fix migration_template

--- lib/generators/smart_authentication/install_generator.rb
+++ lib/generators/smart_authentication/install_generator.rb
@@ -1,3 +1,5 @@
+include Rails::Generators::Migration
+
 module SmartAuthentication
   module Generators
     class InstallGenerator < Rails::Generators::Base
@@ -8,7 +10,16 @@ module SmartAuthentication
         copy_file "app/models/user.rb", "app/models/user.rb"
         copy_file "app/helpers/sessions_helper.rb", "app/helpers/sessions_helper.rb"
         migration_template "db/migrate/create_users.rb", "db/migrate/create_users.rb"
-        insert_into_file "config/routes.rb", "get 'signup' => 'users#new'\nget 'login' => 'sessions#new'\npost 'login' => 'sessions#create'\ndelete 'logout' => 'sessions#destroy'", after: "routes.draw do'\n"
+        insert_into_file "config/routes.rb", "\tget 'signup' => 'users#new'\n\tget 'login' => 'sessions#new'\n\tpost 'login' => 'sessions#create'\n\tdelete 'logout' => 'sessions#destroy'\n", after: "Rails.application.routes.draw do\n"
+      end
+
+      def self.next_migration_number(path)
+        unless @prev_migration_nr
+          @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+        else
+          @prev_migration_nr += 1
+        end
+        @prev_migration_nr.to_s
       end
     end
   end
