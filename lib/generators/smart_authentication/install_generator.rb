ae8e5fb90ba810d97c7fc4db8b920338e2ee5826
Yuki Hariguchi <y.hariguchi@gmail.com>
2015-10-07 07:39:11 +0900

    views model

--- lib/generators/smart_authentication/install_generator.rb
+++ lib/generators/smart_authentication/install_generator.rb
@@ -6,8 +6,17 @@ module SmartAuthentication
       source_root File.expand_path("../templates", __FILE__)
 
       def generate_model
+        insert_into_file "app/controllers/application_controller.rb", "
+          \tinclude SessionsHelper\n\n
+          \t# Confirms a logged-in user.\n
+          \tdef logged_in_user\n
+          \t\tunless logged_in?\n
+          \t\tstore_location\n
+          \t\tflash[:danger] = 'Please log in.'\n
+          \t\tredirect_to login_url\n", after: "protect_from_forgery with: :exception\n"
         copy_file "app/controllers/sessions_controller.rb", "app/controllers/sessions_controller.rb"
         copy_file "app/models/user.rb", "app/models/user.rb"
+        copy_file "app/views/users/new.html.haml", "app/views/users/new.html.haml"
         copy_file "app/helpers/sessions_helper.rb", "app/helpers/sessions_helper.rb"
         migration_template "db/migrate/create_users.rb", "db/migrate/create_users.rb"
         insert_into_file "config/routes.rb", "\tget 'signup' => 'users#new'\n\tget 'login' => 'sessions#new'\n\tpost 'login' => 'sessions#create'\n\tdelete 'logout' => 'sessions#destroy'\n", after: "Rails.application.routes.draw do\n"
--- lib/generators/smart_authentication/templates/app/models/user.rb
+++ lib/generators/smart_authentication/templates/app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
 
-  has_secure_password
+  attr_accessor :remember_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
